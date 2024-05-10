1dea79b8eb9275dc3f4820298b00c6c688cf2d04
Revey <torresdaniel42@gmail.com>
2024-05-10 12:07:52 -0400

    completed exercise 1 and started to brainstorm how to solve exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
 const repeatString = function() {
-
+    let word = "hey";
+    let repeatedWord = "";
+    for (let i = 0; i < 3; i++) {
+        repeatedWord += word;
+    }
+    return repeatedWord;
 };
 
 // Do not edit below this line
